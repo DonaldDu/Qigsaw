7c59da0de4a262b556502184f1cbc2da65f551a0
donald <onsemise@qq.com>
2021-02-25 11:05:45 +0800

    SimpleDownloader ok

--- downloader/src/main/java/com/iqiyi/qigsaw/sample/downloader/SimpleDownloader.kt
+++ downloader/src/main/java/com/iqiyi/qigsaw/sample/downloader/SimpleDownloader.kt
@@ -12,16 +12,18 @@ import com.liulishuo.okdownload.core.listener.DownloadListener3
  * @param threshold default is 10MB
  * @param progressInterval minIntervalMillisCallbackProgress
  * */
-class SimpleDownloader(private val threshold: Long = 10 * 1024 * 1024, private val progressInterval: Int = 150) : Downloader {
+class SimpleDownloader(private val threshold: Long = 10 * 1024 * 1024, private val progressInterval: Int = 150, private val log: Boolean = false) : Downloader {
     private val downloadTasks: MutableMap<Int, Array<DownloadTask>> = mutableMapOf()
     override fun startDownload(sessionId: Int, requests: MutableList<DownloadRequest>, callback: DownloadCallback) {
-        val tasks = requests.map { it.toTask(false, false) }.toTypedArray()
-        DownloadTask.enqueue(tasks, TaskDownloadCallBack(sessionId, callback))
-        downloadTasks[sessionId] = tasks
+        download(sessionId, requests, callback, false, false)
     }
 
     override fun deferredDownload(sessionId: Int, requests: MutableList<DownloadRequest>, callback: DownloadCallback, usingMobileDataPermitted: Boolean) {
-        val tasks = requests.map { it.toTask(true, !usingMobileDataPermitted) }.toTypedArray()
+        download(sessionId, requests, callback, true, !usingMobileDataPermitted)
+    }
+
+    private fun download(sessionId: Int, requests: MutableList<DownloadRequest>, callback: DownloadCallback, deferred: Boolean, wifiRequired: Boolean) {
+        val tasks = requests.map { it.toTask(deferred, wifiRequired) }.toTypedArray()
         DownloadTask.enqueue(tasks, TaskDownloadCallBack(sessionId, callback))
         downloadTasks[sessionId] = tasks
     }
@@ -42,52 +44,79 @@ class SimpleDownloader(private val threshold: Long = 10 * 1024 * 1024, private v
 
     private fun DownloadRequest.toTask(deferred: Boolean, wifiRequired: Boolean): DownloadTask {
         val priority = if (deferred) 0 else 10
-        return DownloadTask.Builder(url, fileDir, fileName)
+        val task = DownloadTask.Builder(url, fileDir, fileName)
                 .setPriority(priority)
                 .setMinIntervalMillisCallbackProcess(progressInterval)
                 .setWifiRequired(wifiRequired)
                 .build()
+        task.tag = DownloadProgress()
+        return task
     }
 
+    private data class DownloadProgress(var currentOffset: Long = 0, var completed: Boolean = false)
+
+    private val DownloadTask.progress: DownloadProgress
+        get() {
+            return tag as DownloadProgress
+        }
+
     private inner class TaskDownloadCallBack(private val sessionId: Int, private val callback: DownloadCallback) : DownloadListener3() {
-        val TAG="DownloadCallback"
+        val TAG = "DownloadCallback"
         override fun retry(task: DownloadTask, cause: ResumeFailedCause) {
-            Log.d(TAG, "retry: ---------------")
+            if (log) Log.d(TAG, "retry: ---------------")
         }
 
         override fun connected(task: DownloadTask, blockCount: Int, currentOffset: Long, totalLength: Long) {
-            Log.d(TAG, "connected: ---------------")
+            task.progress.currentOffset = currentOffset
+            if (log) Log.d(TAG, "connected: ---------------")
         }
 
         override fun progress(task: DownloadTask, currentOffset: Long, totalLength: Long) {
-            callback.onProgress(currentOffset)
-            Log.d(TAG, "progress: ---------------")
+            task.progress.currentOffset = currentOffset
+            callback.onProgress(bunchProgress())
+            if (log) Log.d(TAG, "progress: ---------------,totalLength $totalLength:$currentOffset finish " + (currentOffset == totalLength))
         }
 
+        private fun bunchProgress(): Long {
+            return downloadTasks.getValue(sessionId).sumOf { it.progress.currentOffset }
+        }
+
+        private var started = false
         override fun started(task: DownloadTask) {
-            callback.onStart()
-            Log.d(TAG, "started: ---------------")
+            if (!started) {
+                started = true
+                callback.onStart()
+            }
+            if (log) Log.d(TAG, "started: --------------- ${task.url}")
         }
 
         override fun completed(task: DownloadTask) {
-            downloadTasks.remove(sessionId)
-            callback.onCompleted()
-            Log.d(TAG, "onCompleted: ---------------")
+            task.progress.completed = true
+            if (isCompleted()) {
+                downloadTasks.remove(sessionId)
+                callback.onCompleted()
+            }
+            if (log) Log.d(TAG, "onCompleted: ---------------")
+        }
+
+        private fun isCompleted(): Boolean {
+            return downloadTasks.getValue(sessionId).all { it.progress.completed }
         }
 
         override fun canceled(task: DownloadTask) {
             downloadTasks.remove(sessionId)
             callback.onCanceled()
-            Log.d(TAG, "canceled: ---------------")
+            if (log) Log.d(TAG, "canceled: ---------------")
         }
 
         override fun error(task: DownloadTask, e: Exception) {
+            e.printStackTrace()
             callback.onError(e.hashCode())
-            Log.d(TAG, "error: ---------------")
+            if (log) Log.d(TAG, "error: ---------------")
         }
 
         override fun warn(task: DownloadTask) {
-            Log.d(TAG, "warn: ---------------")
+            if (log) Log.d(TAG, "warn: ---------------")
         }
     }
 }
